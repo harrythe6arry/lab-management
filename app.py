ea704c4e3f841b3168fb1362b5514e5f540e6aaa
Lingling <pusanisa.jar@student.mahidol.edu>
2025-01-30 22:39:57 +0700

    <feat>(<api>): booking frontend + calling availble things works

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from routes import auth, dashboard, tasks, users, inventory
+from routes import auth, dashboard, tasks, users, inventory, booking
 
 app = Flask(__name__)
 app.register_blueprint(auth.auth_routes)
@@ -9,6 +9,7 @@ app.register_blueprint(dashboard.dashboard_routes)
 app.register_blueprint(tasks.tasks_routes)
 app.register_blueprint(users.users_routes)
 app.register_blueprint(inventory.inventory_routes)
+app.register_blueprint(booking.booking_routes)
 
 app.secret_key = os.getenv('SECRET_KEY')
 
