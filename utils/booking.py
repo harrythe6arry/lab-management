9f7eeeefb73e91994f466e647356b082737a4a8e
Lingling <pusanisa.jar@student.mahidol.edu>
2025-01-31 20:25:17 +0700

    <feat>(<api>): finished validate available quantity + day and time slot restriction

--- utils/booking.py
+++ utils/booking.py
@@ -1,19 +1,73 @@
 import psycopg2
 from utils import db
+#
+# def get_available_rooms(selected_date, timeslot):
+#     try:
+#         with db.get_db_connection() as conn:
+#             with conn.cursor() as cur:
+#                 cur.execute("""
+#                       SELECT * FROM rooms
+#                         WHERE id NOT IN (
+#                             SELECT item_id FROM bookings
+#                             WHERE type = %s AND date = %s AND time_slot = %s
+#                         )
+#                 """, ("room", selected_date, timeslot))
+#                 conn.commit()
+#                 return cur.fetchall()
+#     except psycopg2.Error as e:
+#         print(f"[ERROR] Failed to get available room: {e}")
+#         return []
+#
+# def get_available_equipment(selected_date, timeslot):
+#     try:
+#         with db.get_db_connection() as conn:
+#             with conn.cursor() as cur:
+#                 cur.execute("""
+#                       SELECT * FROM equipment
+#                         WHERE id NOT IN (
+#                             SELECT item_id FROM bookings
+#                             WHERE type = %s AND date = %s AND time_slot = %s
+#                         )
+#                 """, ("equipment", selected_date, timeslot))
+#                 conn.commit()
+#                 return cur.fetchall()
+#     except psycopg2.Error as e:
+#         print(f"[ERROR] Failed to get available equipment: {e}")
+#         return []
+#
+# def validate_quantity(equipment_id, selected_date, timeslot):
+#     try:
+#         with db.get_db_connection() as conn:
+#             with conn.cursor() as cur:
+#             # count how many quipment has been book with the same selected date and timeslot
+#                 count_amount_booked_query = """
+#                 SELECT COUNT(*) FROM bookings
+#                 WHERE item_id = %s AND type = %s AND date = %s AND time_slot = %s
+#                 """
+#                 cur.execute(count_amount_booked_query, (equipment_id, "equipment", selected_date, timeslot))
+#                 conn.commit()
+#                 print("count" + cur.fetchone()[0])
+#                 return cur.fetchone()[0]
+#     except psycopg2.Error as e:
+#         print(f"[ERROR] Failed to validate quantity: {e}")
+#         return None
+
+import psycopg2
+from utils import db
+
 
 def get_available_rooms(selected_date, timeslot):
     try:
         with db.get_db_connection() as conn:
             with conn.cursor() as cur:
                 cur.execute("""
-                      SELECT * FROM rooms
-                        WHERE id NOT IN (
-                            SELECT item_id FROM bookings
-                            WHERE type = %s AND date = %s AND time_slot = %s
-                        )
+                    SELECT * FROM rooms
+                    WHERE id NOT IN (
+                        SELECT item_id FROM bookings
+                        WHERE type = %s AND date = %s AND time_slot = %s
+                    )
                 """, ("room", selected_date, timeslot))
-                conn.commit()
-                return cur.fetchall()
+                return cur.fetchall()  # No need for commit()
     except psycopg2.Error as e:
         print(f"[ERROR] Failed to get available room: {e}")
         return []
@@ -24,68 +78,92 @@ def get_available_equipment(selected_date, timeslot):
         with db.get_db_connection() as conn:
             with conn.cursor() as cur:
                 cur.execute("""
-                      SELECT * FROM equipment
-                        WHERE id NOT IN (
-                            SELECT item_id FROM bookings
-                            WHERE type = %s AND date = %s AND time_slot = %s
-                        )
+                    SELECT * FROM equipment
+                    WHERE id NOT IN (
+                        SELECT item_id FROM bookings
+                        WHERE type = %s AND date = %s AND time_slot = %s
+                    )
                 """, ("equipment", selected_date, timeslot))
-                conn.commit()
-                return cur.fetchall()
+                return cur.fetchall()  # No need for commit()
     except psycopg2.Error as e:
         print(f"[ERROR] Failed to get available equipment: {e}")
         return []
 
 
-def validate_quantity(equipment_id, selected_date, timeslot):
+def validate_equipment_quantity(equipment_id, quantity, selected_date, timeslot):
+
+    print(f"equipment_id: {equipment_id}, selected_date: {selected_date}, timeslot: {timeslot}, quantity: {quantity}")
+
     try:
+        # Query the database to check available quantity
         with db.get_db_connection() as conn:
             with conn.cursor() as cur:
                 cur.execute("""
-                      SELECT quantity FROM equipment
-                        WHERE id = %s AND date = %s AND time_slot = %s
+                    SELECT total_quantity FROM equipment WHERE id = %s
+                """, (equipment_id,))
+                available_quantity = cur.fetchone()[0]
+
+                cur.execute("""
+                    SELECT COUNT(*) FROM bookings
+                    WHERE item_id = %s AND type = 'equipment' AND date = %s AND time_slot = %s
                 """, (equipment_id, selected_date, timeslot))
-                conn.commit()
-                return cur.fetchone()
+                booked_quantity = cur.fetchone()[0]
+                is_available = available_quantity - booked_quantity >= quantity
+                db.close_db_connection(conn)
+                return True if is_available else False
     except psycopg2.Error as e:
         print(f"[ERROR] Failed to validate quantity: {e}")
         return None
 
-def deduct_equipment_quantity(equipment_name, quantity, date, timeslot):
-    # Check if the quantity is available in the database for the given date and timeslot
-    query = """
-    SELECT quantity FROM equipment_table
-    WHERE name = %s AND date = %s AND timeslot = %s;
-    """
-    result = db.execute(query, (equipment_name, date, timeslot))
-    if result and result[0]['total_quantity'] >= quantity:
-        # Deduct the quantity
-        update_query = """
-        UPDATE equipment_table
-        SET quantity = quantity - %s
-        WHERE name = %s AND date = %s AND timeslot = %s;
-        """
-        db.execute(update_query, (quantity, equipment_name, date, timeslot))
-        return True
-    return False
 
-def book_room_and_equipment(name, room, equipment, date, timeslot):
+
+def book_room_and_equipment(name, room_id, equipments, date, timeslot):
     # Book the room and equipment here
     # Example: Insert booking data into the database for the room
-    booking_query = """
-    INSERT INTO bookings (name, room, date, timeslot)
-    VALUES (%s, %s, %s, %s);
-    """
-    db.execute(booking_query, (name, room, date, timeslot))
+    # booking_query = """
+    # INSERT INTO bookings (name, type, date, timeslot, quantity, item_id)
+    # VALUES (%s, %s, %s, %s, %s, %s);
+    # """
+    # db.execute(booking_query, (name, "room", date, timeslot, 1, room_id))
+    #
+    # # Insert equipment bookings
+    # for eq in equipments:
+    #     booking_equipment_query = """
+    #     INSERT INTO booking (name, type, date, timeslot, quantity, item_id)
+    #     VALUES (%s, %s, %s, %s, %s, %s);
+    #     """
+    #     db.execute(booking_equipment_query, (name, "equipment", date, timeslot, eq[1], room_id))
 
-    # Insert equipment bookings
-    for eq in equipment:
-        equipment_query = """
-        INSERT INTO equipment_bookings (booking_id, equipment_name, quantity)
-        VALUES (LAST_INSERT_ID(), %s, %s);
-        """
-        db.execute(equipment_query, (eq['name'], eq['quantity']))
+    # print all input data
+    print(f"Booking room '{room_id}' for '{name}' on {date} at {timeslot}")
+    print(f"Booking equipment: {equipments}")
 
-    return True
+    try:
+        with db.get_db_connection() as conn:
+            with conn.cursor() as cur:
+                # Insert room booking and return room ID
+                cur.execute("""
+                      INSERT INTO bookings (name, type, date, timeslot, quantity, item_id)
+                      VALUES (%s, %s, %s, %s, %s, %s) RETURNING id;
+                  """, (name, "room", date, timeslot, 1, room_id))
+                room_booking_id = cur.fetchone()[0]  # Fetch the returned room booking ID
+
+                # Insert equipment bookings and collect their IDs
+                equipment_ids = []
+                for eq in equipments:
+                    equipment_name, quantity = eq
+                    cur.execute("""
+                        INSERT INTO bookings (name, type, date, timeslot, quantity, item_id)
+                        VALUES (%s, %s, %s, %s, %s, %s) RETURNING id;
+                    """, (name, "equipment", date, timeslot, quantity, equipment_name))
+                    equipment_ids.append(cur.fetchone()[0])
+
+                conn.commit()
+                db.close_db_connection(conn)
+                return room_booking_id, equipment_ids
+    except psycopg2.Error as e:
+        print(f"[ERROR] Failed to book room and equipment: {e}")
+        return None, []
+
 
 
